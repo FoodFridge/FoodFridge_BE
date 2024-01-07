72ac86c8c556431fbf4991db62ca191620d25998
Wararat <princess_princeshin@hotmail.com>
2024-01-07 16:59:30 -0500

    initial commit

---
+++ firebase.py
@@ -0,0 +1,12 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+#authenticate to firebase
+cred = credentials.Certificate("firebase_credentials.json")
+firebase_admin.initialize_app(cred)
+
+#storing client db data to firebase
+#db = firestore.client()
+#db.collection("person").add({'name': 'John', 'age': '40'})
+
