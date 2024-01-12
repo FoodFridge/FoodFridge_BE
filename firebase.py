f953c04a8098f22b1c53dda8735c1aaae7277dd2
jessie <jessie.pastan@gmail.com>
2024-01-12 00:38:17 -0500

    implement get ingredients api endpoint

--- firebase.py
+++ firebase.py
@@ -7,6 +7,7 @@ cred = credentials.Certificate("firebase_credentials.json")
 firebase_admin.initialize_app(cred)
 
 #storing client db data to firebase
-#db = firestore.client()
-#db.collection("person").add({'name': 'John', 'age': '40'})
+db = firestore.client()
+# db.collection("person").add({'name': 'Johny', 'age': '40'})
+db.collection("ingredient").add({'ingredient_id': '40', 'ingredient_name': 'Grains', 'ingredient_type_code': '01' })
 
