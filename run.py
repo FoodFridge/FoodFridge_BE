fb4d9deebe343bad3d19bdba1a25d623165be0da
WijiPe <93677775+WijiPe@users.noreply.github.com>
2024-01-24 15:12:42 -0800

    get/post favorite

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask_restful import Api
 from app.core.firebase import initialize_firebase_app, firestore
 from app.api.v1.routes.resources.alpha_resource import AlphaResource
 from app.api.v1.routes.resources.ingredient_resource import IngredientResource ,IngredientResourceWithCategory
-from app.api.v1.routes.resources.recipe_resource import RecipeResource
+from app.api.v1.routes.resources.favorite_resource import AddFavoriteResource, FavoriteResourceByUser
 
 app = Flask(__name__)
 api = Api(app)  # Initialize the Api object
@@ -16,8 +16,8 @@ initialize_firebase_app()
 api.add_resource(AlphaResource, '/api/v1/aplha/<string:type>')
 api.add_resource(IngredientResource, '/api/v1/ingredient/')
 api.add_resource(IngredientResourceWithCategory, '/api/v1/ingredient/<string:category>')
-api.add_resource(RecipeResource, '/api/v1/recipe/add')
-
+api.add_resource(AddFavoriteResource, '/api/v1/favorite')
+api.add_resource(FavoriteResourceByUser, '/api/v1/favorite/<string:user_id>')
 if __name__ == '__main__':
     app.run(debug=True)
 
