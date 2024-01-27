c5f89e116a31f34f426830da1e765aa8d7bf41ae
WijiPe <93677775+WijiPe@users.noreply.github.com>
2024-01-26 21:20:36 -0800

    pantry/ add favorite

--- app/api/v1/routes/resources/favorite_resource.py
+++ app/api/v1/routes/resources/favorite_resource.py
@@ -2,8 +2,21 @@ from flask import request, jsonify
 from flask_restful import Resource
 from firebase_admin import auth, initialize_app, credentials
 from app.core.firebase import initialize_firebase_app, firestore
+import secrets
+import random
+import string
 # import logging
 
+def generate_random_string(length=8):
+    return secrets.token_hex(length // 2)
+
+def generate_random_integer(min_value=1, max_value=100):
+    return random.randint(min_value, max_value)
+
+def generate_random_string_and_integer():
+    random_string = generate_random_string()
+    random_integer = generate_random_integer()
+    return f"{random_string}_{random_integer}"
 
 class FavoriteResourceByUser(Resource):
     def get(self, user_id):
@@ -52,26 +65,23 @@ class FavoriteResourceByUser(Resource):
 
 
 class AddFavoriteResource(Resource):
-    #to do find user id 
-    def post(self):
+    def post(self, user_id):
         try:
             db = firestore.client()
 
             data = request.get_json()
 
             img = data.get('img')
-            status = data.get('status')
             recipeName = data.get('recipeName')
             title = data.get('title')
             url = data.get('url')
-            user_id = data.get('user_id')
             print(recipeName)
             collection_ref = db.collection('favorite')
             document_id = collection_ref.document().id
-
           
             favorite= {
-                "status" : status,
+                "status" : "N",
+                "fav_id" : generate_random_string_and_integer(),
                 'img': img,
                 'recipeName': recipeName,
                 'title': title,
@@ -80,10 +90,76 @@ class AddFavoriteResource(Resource):
                 # Add more fields as needed
             }
 
-            
             collection_ref.document(document_id).set(favorite)
 
             return {"success": f"Document {document_id} added to collection 'Favorite'", "document_id": document_id}
+
+        except Exception as e:
+            # Handle the exception and return an appropriate response
+            error_message = f"An error occurred: {str(e)}"
+            return {"error": error_message}, 500
+
+        #จำเป็นไหม? ส่ง Document Id ยังไง และ ถ้าไม่ใช้ fav id ให้กลับไปแก้
+class FavoriteResourceByFavID(Resource):
+    def get(self, user_id, fav_id):
+        try:
+            db = firestore.client()
+
+            collection_ref = db.collection('favorite')
+            docs = collection_ref.stream()
+
+            data = []
+            for doc in docs:
+                data.append(doc.to_dict())
+
+            if data:
+
+                filtered_data = [
+                    {"img": item.get("img"), "title": item.get("title"), "recipeName": item.get("recipeName"), "status": item.get("status") , 
+                     "url": item.get("url")}
+                    for item in data if item.get("user_id") == user_id and item.get("fav_id") == fav_id
+                ]
+
+                print(filtered_data)
+
+                response = {
+                    "status": "1",
+                    "message": "Data retrieved successfully",
+                    "data": filtered_data
+                }
+            else:
+                # If no data is present, return a response with a message
+                response = {
+                    "status": "0",
+                    "message": "No data available",
+                    "data": []
+                }
+            return response, 200
+        
+        except Exception as e:
+            # Handle the exception and return an appropriate response
+            error_message = f"An error occurred: {str(e)}"
+            return {"error": error_message}, 500
+        
+class ChangeFavoriteResource(Resource):
+    def put(self):
+        try:
+            db = firestore.client()
+
+            data = request.get_json()
+
+            document_id = data.get('document_id')
+
+            if not document_id:
+                return {"error": "Missing required parameters"}, 400
+            
+            collection_name = 'favorite'
+            doc_ref = db.collection(collection_name).document(document_id)
+            current_status = doc_ref.get().get('status')
+            new_status = 'N' if current_status == 'Y' else 'Y'
+
+            doc_ref.update({'status': new_status})
+            return {"success": f"Favorite status toggled for document ID {document_id}. New status: {new_status}"}
 
         except Exception as e:
             # Handle the exception and return an appropriate response
--- run.py
+++ run.py
@@ -4,9 +4,10 @@ from flask_restful import Api
 from app.core.firebase import initialize_firebase_app, firestore
 from app.api.v1.routes.resources.alpha_resource import AlphaResource
 from app.api.v1.routes.resources.ingredient_resource import IngredientResource ,IngredientResourceWithCategory
-from app.api.v1.routes.resources.favorite_resource import AddFavoriteResource, FavoriteResourceByUser
+from app.api.v1.routes.resources.favorite_resource import AddFavoriteResource, FavoriteResourceByUser, FavoriteResourceByFavID, ChangeFavoriteResource
 from app.api.v1.routes.resources.recipe_resource import GenerateRecipeFromIngredients
 from app.api.v1.routes.resources.google_resource import SearchWithRecipe
+from app.api.v1.routes.resources.pantry_resource import PantryResourceByUser, AddPantryResource
 
 app = Flask(__name__)
 api = Api(app)  # Initialize the Api object
@@ -18,10 +19,15 @@ initialize_firebase_app()
 api.add_resource(AlphaResource, '/api/v1/aplha/<string:type>')
 api.add_resource(IngredientResource, '/api/v1/ingredient/')
 api.add_resource(IngredientResourceWithCategory, '/api/v1/ingredient/<string:category>')
-api.add_resource(AddFavoriteResource, '/api/v1/favorite')
+api.add_resource(AddFavoriteResource, '/api/v1/favorite/add/<string:user_id>')
 api.add_resource(FavoriteResourceByUser, '/api/v1/favorite/<string:user_id>')
+api.add_resource(FavoriteResourceByFavID, '/api/v1/favorite/<string:user_id>/<string:fav_id>')
+api.add_resource(ChangeFavoriteResource, '/api/v1/favorite/change')
 api.add_resource(GenerateRecipeFromIngredients, '/api/v1/GenerateRecipe/')
 api.add_resource(SearchWithRecipe, '/api/v1/search/<string:recipeName>')
+api.add_resource(AddPantryResource, '/api/v1/pantry/add/<string:user_id>')
+api.add_resource(PantryResourceByUser, '/api/v1/pantry/<string:user_id>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
