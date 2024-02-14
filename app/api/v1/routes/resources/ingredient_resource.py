54ef1544e4d531d5ab798567d1b5c4ccd1d7e47a
WijiPe <93677775+WijiPe@users.noreply.github.com>
2024-02-13 20:05:04 -0800

    deleted  Not specified

--- app/api/v1/routes/resources/ingredient_resource.py
+++ app/api/v1/routes/resources/ingredient_resource.py
@@ -41,7 +41,7 @@ class IngredientResource(Resource):
                 doc_dict2 = doc.to_dict()
 
                 pantry = {
-                    "user_id": doc_dict.get("user_id", "Not specified"),
+                    "user_id": doc_dict.get("user_id"),
                     "doc_id": doc.id,
                     "ingredient_name": doc_dict2.get("pantryName"),
                     "ingredient_type_code": doc_dict2.get("ingredient_type_code"),
