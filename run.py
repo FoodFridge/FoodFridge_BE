d74f7605dea9230ee271b1ceda80f9f8260a6853
WijiPe <93677775+WijiPe@users.noreply.github.com>
2024-02-27 13:57:15 -0800

    uncomment awsgi

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ from app.api.v1.routes.resources.link_recipe_resource import LinkRecipeResource
 from app.api.v1.routes.resources.pantry_resource import PantryResourceByUser, AddPantryResource, EditPantryResource, DeletePantryResource
 
 from app.api.v1.routes.resources.users import Login_with_email_and_password, Logout, Sign_up_with_email_and_password,LoginWithGoogle,SignUpWithGoogle, Update_Name, Update_Password
-# import awsgi
+import awsgi
 
 app = Flask(__name__)
 api = Api(app)
